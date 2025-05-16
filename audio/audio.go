40e79b11775b3b5fc903b60ee9addd72e43c50ba
Matúš Ollah <matusollah420@gmail.com>
2025-05-16 22:39:36 +0200

    add audio utils

---
+++ audio/audio.go
@@ -0,0 +1,33 @@
+// Package audio implements functions for the manipulation of sample slices and audio.
+package audio
+
+func Interleave(samples [][]float64) []float64 {
+	out := []float64{}
+	for i := range samples {
+		for j := range samples[i] {
+			out = append(out, samples[i][j])
+		}
+	}
+	return out
+}
+
+func Deinterleave(interleaved []float64, numChannels int) [][]float64 {
+	if numChannels <= 0 {
+		panic("audio: number of channels must be positive")
+	}
+
+	out := make([][]float64, numChannels)
+	totalFrames := len(interleaved) / numChannels
+
+	for i := range out {
+		out[i] = make([]float64, totalFrames)
+	}
+
+	for i := range totalFrames {
+		for ch := range numChannels {
+			out[ch][i] = interleaved[i*numChannels+ch]
+		}
+	}
+
+	return out
+}
