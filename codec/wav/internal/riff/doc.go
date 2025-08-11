23e8436b43b4a819c02e1efcd60e6495cfe92be6
Matúš Ollah <matusollah420@gmail.com>
2025-08-11 20:26:35 +0200

    Add license and attribution for riff package

--- codec/wav/internal/riff/doc.go
+++ codec/wav/internal/riff/doc.go
@@ -1,2 +1,7 @@
+// Portions of this code are derived from golang.org/x/image/riff
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package riff implements the Resource Interchange File Format (RIFF), used by WAVE.
 package riff
