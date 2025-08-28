615e4201139c31c69f99d4ea7e366613e57cf52e
Matúš Ollah <matusollah420@gmail.com>
2025-08-28 15:09:01 +0200

    Add attribution for aio package

--- aio/aio.go
+++ aio/aio.go
@@ -1,3 +1,8 @@
+// Portions of this code are derived from the Go standard library's io package.
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package aio defines basic interfaces for audio I/O primitives.
 // It provides shared abstractions over various audio backends,
 // enabling modular and interoperable audio processing components.
