69fb59be95b03bb6d0b74a4f68cb29ad95553c36
Matúš Ollah <57183251+MatusOllah@users.noreply.github.com>
2025-09-18 10:28:59 +0200

    Add Upmixer

---
+++ audio/upmixer.go
@@ -0,0 +1,49 @@
+package audio
+
+import (
+	"io"
+
+	"github.com/MatusOllah/resona/aio"
+)
+
+// Upmixer duplicates samples and converts mono to multiple-channel audio (e.g. stereo, 5.1 surround).
+type Upmixer struct {
+	src      aio.SampleReader
+	numChans int
+	monoBuf  []float64
+}
+
+// NewUpmixer creates a new [Upmixer] with the specified target number of channels to upmix to.
+func NewUpmixer(r aio.SampleReader, numChans int) *Upmixer {
+	return &Upmixer{
+		src:      r,
+		numChans: numChans,
+	}
+}
+
+func (u *Upmixer) ReadSamples(p []float64) (int, error) {
+	if len(p)%u.numChans != 0 {
+		return 0, io.ErrShortBuffer
+	}
+
+	monoSamples := len(p) / u.numChans
+
+	if cap(u.monoBuf) < monoSamples {
+		u.monoBuf = make([]float64, monoSamples)
+	} else {
+		u.monoBuf = u.monoBuf[:monoSamples]
+	}
+
+	n, err := u.src.ReadSamples(u.monoBuf)
+	if err != nil && err != io.EOF {
+		return 0, err
+	}
+
+	for i := range u.monoBuf {
+		for ch := range u.numChans {
+			p[i*u.numChans+ch] = u.monoBuf[i]
+		}
+	}
+
+	return n * u.numChans, err
+}
---
+++ audio/upmixer_test.go
@@ -0,0 +1,25 @@
+package audio_test
+
+import (
+	"testing"
+
+	"github.com/MatusOllah/resona/audio"
+	"github.com/MatusOllah/resona/internal/testutil"
+)
+
+func TestUpmixer(t *testing.T) {
+	samples := []float64{0.1, 0.2, 0.3}
+	want := []float64{0.1, 0.1, 0.2, 0.2, 0.3, 0.3}
+
+	upmix := audio.NewUpmixer(audio.NewReader(samples), 2)
+
+	got := make([]float64, len(samples)*2)
+	_, err := upmix.ReadSamples(got)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !testutil.EqualSliceWithinTolerance(want, got, 1e-12) {
+		t.Errorf("upmixed samples do not match: got %v, want %v", got, want)
+	}
+}
