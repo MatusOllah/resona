ed16d98fa96fd8278d4f2aa5f5d5e39a96a665e7
Matúš Ollah <57183251+MatusOllah@users.noreply.github.com>
2025-09-30 10:39:36 +0200

    Remove pausing in playback Player

--- playback/player.go
+++ playback/player.go
@@ -10,14 +10,14 @@ import (
 // Player represents an audio player.
 type Player struct {
 	mux *audio.Mixer
-	src *pausable
+	src aio.SampleReader
 }
 
 // NewPlayer creates a new [Player].
 func (ctx *Context) NewPlayer(src aio.SampleReader) *Player {
 	return &Player{
 		mux: ctx.mux,
-		src: &pausable{r: src},
+		src: src,
 	}
 }
 
@@ -50,29 +50,4 @@ func (p *Player) PlayWithDone() chan struct{} {
 // PlayAndWait starts the playback and blocks until the player has finished playing and drained.
 func (p *Player) PlayAndWait() {
 	<-p.PlayWithDone()
-}
-
-// Pause pauses the playback.
-func (p *Player) Pause() {
-	p.src.paused = true
-}
-
-// Unpause resumes the playback.
-func (p *Player) Unpause() {
-	p.src.paused = false
-}
-
-// pausable is a wrapper around a regular aio.SampleReader that adds pausing/unpausing capabilities.
-// It outputs silence when paused.
-type pausable struct {
-	r      aio.SampleReader
-	paused bool
-}
-
-func (r *pausable) ReadSamples(p []float64) (int, error) {
-	if r.paused {
-		clear(p)
-		return len(p), nil
-	}
-	return r.r.ReadSamples(p)
 }
