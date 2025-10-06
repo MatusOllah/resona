5a166c773e0fef7eef21a82dee8e726b0b19f820
Matúš Ollah <57183251+MatusOllah@users.noreply.github.com>
2025-10-06 09:51:04 +0200

    Add afmt example

---
+++ afmt/example_test.go
@@ -0,0 +1,20 @@
+package afmt_test
+
+import (
+	"encoding/binary"
+	"fmt"
+
+	"github.com/MatusOllah/resona/afmt"
+)
+
+func ExampleSampleFormat_String() {
+	fmt.Println("8-bit unsigned PCM:", afmt.SampleFormat{BitDepth: 8, Encoding: afmt.SampleEncodingUint}.String())
+	fmt.Println("16-bit signed big-endian PCM:", afmt.SampleFormat{BitDepth: 16, Encoding: afmt.SampleEncodingInt, Endian: binary.BigEndian}.String())
+	fmt.Println("24-bit signed PCM:", afmt.SampleFormat{BitDepth: 24, Encoding: afmt.SampleEncodingInt}.String())
+	fmt.Println("32-bit float little-endian PCM:", afmt.SampleFormat{BitDepth: 32, Encoding: afmt.SampleEncodingFloat, Endian: binary.LittleEndian}.String())
+	// Output:
+	// 8-bit unsigned PCM: uint8
+	// 16-bit signed big-endian PCM: int16be
+	// 24-bit signed PCM: int24
+	// 32-bit float little-endian PCM: float32le
+}
