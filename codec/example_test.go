912dee786e894e1a89f477d8db0810063600ea5b
Matúš Ollah <57183251+MatusOllah@users.noreply.github.com>
2025-10-15 11:28:47 +0200

    Add example for codec package

---
+++ codec/example_test.go
@@ -0,0 +1,30 @@
+package codec_test
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/MatusOllah/resona/codec"
+	_ "github.com/MatusOllah/resona/codec/flac" // Enable FLAC decoder
+)
+
+func Example() {
+	f, err := os.Open("file.flac")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	dec, name, err := codec.Decode(f)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(name) // Should print "flac"
+	_ = dec           // Do something with the audio
+
+	// Print out bitrate if the decoder supports it
+	if bitrater, ok := dec.(codec.Bitrater); ok {
+		fmt.Printf("Bitrate: %d kbps\n", bitrater.Bitrate()/1000)
+	}
+}
